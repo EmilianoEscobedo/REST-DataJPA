d37da278c1ff1b97dd3ce7c1870bc54898743a64
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-01-04 15:17:14 -0300

    Created book entity

--- src/main/java/com/example/RESTDataJPA/Book.java
+++ src/main/java/com/example/RESTDataJPA/Book.java
@@ -1,5 +1,105 @@
 package com.example.RESTDataJPA;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
 
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "books")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private Integer pages;
+    private double price;
+    private LocalDate date;
+    private Boolean online;
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, String author, Integer pages, double price, LocalDate date, Boolean online) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.price = price;
+        this.date = date;
+        this.online = online;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Boolean getOnline() {
+        return online;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setOnline(Boolean online) {
+        this.online = online;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", pages=" + pages +
+                ", price=" + price +
+                ", date=" + date +
+                ", online=" + online +
+                '}';
+    }
 }
